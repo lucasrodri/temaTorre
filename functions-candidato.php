b8bd80bc1b0464a38dc6f525173e1fc487a51a26
Lucas Costa <lucasrc.rodri@gmail.com>
2022-08-12 12:11:27 -0300

    ajustando a data

--- functions-candidato.php
+++ functions-candidato.php
@@ -33,7 +33,9 @@ function candidato_view()
                     $entradas_id[$form_id] = $entrada; //novo array para guardar as entry_id
                     $form = Caldera_Forms_Forms::get_form($form_id);
                     $entradas[$form_id] = new Caldera_Forms_Entry($form, $entrada);
-                    $entradas["date"] = $umaEntrada['_date'];
+                    if ($form_id == FORM_ID_GERAL){
+                        $entradas["date"] = $umaEntrada['_date'];
+                    }
                     //TODO levar em consideração versionamento
                     break;
                 }
